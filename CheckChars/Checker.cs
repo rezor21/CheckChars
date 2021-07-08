b92f5ba25968fbb4adee9e2f5bd1916be7cc6e0a
Michał Rembisz <mrembisz@enfoglobe.com>
2021-07-08 18:21:57 +0200

    Dodaj pliki projektów.

---
+++ CheckChars/Checker.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CheckChars
+{
+    public class Checker
+    {
+       
+    }
+}
---
+++ TestCheckChars/TestChecker.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CheckChars;
+
+namespace TestCheckChars
+{
+    [TestClass]
+    public class TestChecker
+    {
+       
+    }
+}
